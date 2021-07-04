0fd9069e1598471940ece6916625c1269f1eee72
Francesc Busquets <francesc@gmail.com>
2021-07-04 12:38:32 +0200

    :arrow_up: Upgrade to CRA 4

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders EUPL link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Llicència Pública de la Unió Europea/i);
+  expect(linkElement).toBeInTheDocument();
 });
